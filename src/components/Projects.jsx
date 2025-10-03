475596df15fed71e689838d4624b5dc5aafa1fa9
honest <honestdev7@gmail.com>
2025-10-03 19:26:43 +0330

    add website restaurant

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -522,6 +522,34 @@ const projectsData = [
       },
     ],
   },
+  {
+    name: "My Restaurant",
+    url: "https://hamiparsa.github.io/My-Restaurant/",
+    images: [
+      "https://raw.githubusercontent.com/HamiParsa/My-Restaurant/main/pic1.png",
+      "https://raw.githubusercontent.com/HamiParsa/My-Restaurant/main/pic2.png",
+      "https://raw.githubusercontent.com/HamiParsa/My-Restaurant/main/pic3.png",
+      "https://raw.githubusercontent.com/HamiParsa/My-Restaurant/main/pic4.png",
+    ],
+    tech: [
+      {
+        name: "React",
+        icon: <FaReact className="text-sky-400 text-2xl sm:text-3xl" />,
+      },
+      {
+        name: "Next.js",
+        icon: <RiNextjsFill className="text-white text-2xl sm:text-3xl" />,
+      },
+      {
+        name: "TypeScript",
+        icon: <SiTypescript className="text-blue-500 text-2xl sm:text-3xl" />,
+      },
+      {
+        name: "Tailwind CSS",
+        icon: <SiTailwindcss className="text-cyan-400 text-2xl sm:text-3xl" />,
+      },
+    ],
+  },
 ];
 
 // Component for individual project slider
